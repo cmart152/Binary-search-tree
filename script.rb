358d02edd8370d12a80985ab1c1de69dcae2f3b6
Chester Martin <chester.martin89@gmail.com>
2022-05-20 13:23:00 +0100

    Added complete script to test binary search tree class

--- script.rb
+++ script.rb
@@ -1,16 +1,28 @@
 require_relative './lib/tree'
 
-arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324, 34, 36, 79, 23]
+arr = [97, 9, 12, 46, 84, 23, 57, 49, 65, 1, 20, 2, 24, 61, 15]
 
 number_tree = Tree.new(arr)
 number_tree.pretty_print
 p number_tree.balanced?
+p number_tree.post_order
+p number_tree.pre_order
+p number_tree.level_order
+p number_tree.in_order
+number_tree.insert(34)
+number_tree.insert(56)
+number_tree.insert(789)
+number_tree.insert(2)
+number_tree.insert(22)
+number_tree.insert(12)
 number_tree.insert(4)
-number_tree.insert(456)
-number_tree.insert(587)
-number_tree.insert(543)
+number_tree.insert(5)
 number_tree.pretty_print
 p number_tree.balanced?
 number_tree.rebalance
 number_tree.pretty_print
 p number_tree.balanced?
+p number_tree.post_order
+p number_tree.pre_order
+p number_tree.level_order
+p number_tree.in_order
